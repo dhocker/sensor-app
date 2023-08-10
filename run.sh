d24acc60ee3bb5d7a690275bb766f48b51af10a9
Dave Hocker <AtHomeX10@gmail.com>
2023-08-10 15:15:18 -0500

    Switch to running the wx versions

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Start sensor-app
-cd /home/pi/rpi/sensor-app
+cd /home/pi/rpi/wx-sensor-app
 # Put the venv at the front of the path
 # When its python is run, it will activate the venv
 PATH=~/Virtualenvs/sensor-app3/bin:$PATH
-python sensor_app.py
+python wx_sensor_app.py
